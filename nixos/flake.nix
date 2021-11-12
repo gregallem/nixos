9bef899e7112913e522275ad10548b5d4bb73fa3
gregallem <greg.allen54@gmail.com>
2021-11-12 15:54:50 +1100

    nixos rename

---
+++ nixos/flake.nix
@@ -0,0 +1,16 @@
+{
+description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.05";
+  };
+
+
+  outputs = { self, nixpkgs }: {
+    # replace 'joes-desktop' with your hostname here.
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+       system = "x86_64-linux";
+       modules = [ ./configuration.nix ];
+    };
+  };
+}
